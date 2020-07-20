c0976a3132cef83974a93ab60534e37cee918bed
Federico Rivero M. <federico.rivero.m@gmail.com>
2020-07-19 20:22:03 -0400

    File to test Github version control

---
+++ src/githubtest/GithubTest.java
@@ -0,0 +1,17 @@
+
+package githubtest;
+
+/**
+ * Git and Github test
+ * @author Federico
+ */
+public class GithubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+    
+}
