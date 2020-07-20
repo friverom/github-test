495868ad88809250c41018bd527cd591dae5d676
Federico Rivero M. <federico.rivero.m@gmail.com>
2020-07-19 20:40:22 -0400

    Added Lower Case print method

--- src/githubtest/PrintClass.java
+++ src/githubtest/PrintClass.java
@@ -9,5 +9,10 @@ public class PrintClass {
 
 public static void printString(String s){
     System.out.println(s);
-}    
+}   
+
+public static void printLowerCase(String s){
+    String t=s.toLowerCase();
+    System.out.println(t);
+}
 }
